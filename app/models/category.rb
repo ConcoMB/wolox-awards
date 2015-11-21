5a8136f5974a5e050068b0ea76e4a2db0147c224
Conco MB <cmaderblanco@wolox.com.ar>
2015-11-21 20:59:08 -0300

    fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,10 +3,5 @@ class Category < ActiveRecord::Base
   has_many :nominees
   has_many :voters
   mount_uploader :avatar, PictureUploader
-  after_create :init
 
-  def init
-    return unless is_open
-    User.all.each { |u| Nominee.create(user: u, category: self) }
-  end
 end
